c7a35900cb6990950da36409cb40ec62a63ba790
Pipecontreras02 <pipecontreras2@gmail.com>
2022-02-09 23:33:04 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse = document.createElement("p");
+  excuse.innerHTML =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 4)] +
+    " " +
+    when[Math.floor(Math.random() * 4)];
+
+  let container = document.getElementById("container");
+  container.append(excuse);
 };
